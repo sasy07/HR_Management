f1fe95d9bb1d6632d3174b71b458458225f2c892
Sasy07 <sasan.ghafooryfard@gmail.com>
2024-11-28 12:21:32 +0330

    init

---
+++ HR_Management.Domain/LeaveAllocation.cs
@@ -0,0 +1,11 @@
+using HR_Management.Domain.Common;
+
+namespace HR_Management.Domain;
+
+public class LeaveAllocation:BaseDomainEntity
+{
+    public int NumberOfDays { get; set; }
+    public LeaveType LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    public int Priod { get; set; }
+}
---
+++ HR_Management.Domain/LeaveType.cs
@@ -0,0 +1,9 @@
+using HR_Management.Domain.Common;
+
+namespace HR_Management.Domain;
+
+public class LeaveType:BaseDomainEntity
+{
+    public string Name { get; set; }
+    public int DefaultDay { get; set; }
+}
